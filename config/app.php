80e0740e6e69e76050e6024b625ff7749be2888b
强总 <qijieiot@163.com>
2025-09-28 01:35:18 +0800

    更新readme

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ return [
     ],
     'http' => 'http://127.0.0.1:9103', //监听http请求 的端口
     'product_key' => 'iot', // 产品key，和mqtt主题有关
-    'mqtt' => 'mqtt://flower.qijieiot.com:1883', //mqtt 服务端地址
+    'mqtt' => 'mqtt://xxx.xxxx.com:1883', //mqtt 服务端地址
     'mqtt_cfg'=>[
         'username'=>'',
         'password'=>''
